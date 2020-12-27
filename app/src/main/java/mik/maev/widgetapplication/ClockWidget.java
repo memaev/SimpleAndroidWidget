2b9fdeaab5a8f854fc3328d90d316da8ed6a198d
memaev <57516381+memaev@users.noreply.github.com>
2020-12-27 17:04:52 +0300

    Initial commit

---
+++ app/src/main/java/mik/maev/widgetapplication/ClockWidget.java
@@ -0,0 +1,73 @@
+package mik.maev.widgetapplication;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.widget.RemoteViews;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class ClockWidget extends AppWidgetProvider {
+
+    public static Bitmap BuildUpdate(String txttime, int size, Context context){
+        Paint paint = new Paint();
+        paint.setTextSize(size);
+        Typeface custom = Typeface.createFromAsset(context.getAssets(), "fonts/Lato-Light.ttf");
+        paint.setTypeface(custom);
+        paint.setColor(Color.WHITE);
+        paint.setTextAlign(Paint.Align.LEFT);
+        paint.setSubpixelText(true);
+        paint.setAntiAlias(true);
+        float baseline = -paint.ascent();
+        int width = (int) (paint.measureText(txttime)+0.5f);
+        int height = (int) (baseline+paint.descent()+0.5f);
+        Bitmap image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_4444);
+        Canvas canvas = new Canvas(image);
+        canvas.drawText(txttime, 0, baseline, paint);
+        return image;
+    }
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.clock_widget);
+        Date dateNow = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("HH:mm");
+        SimpleDateFormat formatForDateNow_date = new SimpleDateFormat("dd.MM.yyyy");
+        views.setImageViewBitmap(R.id.imgtime, BuildUpdate(formatForDateNow.format(dateNow), 200, context));
+        views.setImageViewBitmap(R.id.imgdate, BuildUpdate(formatForDateNow_date.format(dateNow), 60, context));
+
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
